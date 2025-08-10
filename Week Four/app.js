d959cf71599f863658284e14170b30d1922f19fc
unknown <sulaimonalaminakanbi@gmail.com>
2025-08-10 12:08:09 +0100

    Stepping into Java Script

---
+++ Week Four/app.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+let a = 5
+let b = 10
+// Additon.
+let c = a + b
+console.log(c);
+// Using Concatenation
+// console.log("The result of the addition is " + c);
+
+// Addition of String and Variable
+console.log("The addition of " + a + " and " + b + " is "+ c);
+
+// Subtraction. 
+let d = b - a
+console.log(d);
+// console.log("The result of the subtraction is " + d);
+
+// Using Literal String
+console.log(`The subtraction of ${b} and ${a} is ${d}`);
---
+++ Week Four/app2.js
@@ -0,0 +1,18 @@
+console.log("Hello Al Amin");
+
+let x = 4
+let y = 8
+let z = 4 * 8
+let w = 8 / 4
+
+console.log(w);
+console.log(z);
+
+console.log(`The multiplication of ${x} and ${y} is ${z}`);
+console.log("The division of " + y + " by " + x + " is " + w);
+
+console.log("The result of the multiplication is " + z);
+console.log("The result of the division is " + w);
+
+
+
